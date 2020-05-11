ca2df7e00be233a3bbc7ac1ef11f0d8e1e7fccc9
Daniel-Lagos01 <d.f.l.m.0126@gmail.com>
2020-05-11 18:03:29 -0500

    Hello again

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+		Log.d("daniel","OncCreate Function")
+
     }
 
 }
