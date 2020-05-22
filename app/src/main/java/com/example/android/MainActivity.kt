c5b90e14fc47ab9df4c4b6896d930d035f5a4cbc
Daniel-Lagos01 <d.f.l.m.0126@gmail.com>
2020-05-21 19:24:44 -0500

    Create a viewmodel and generate insult from web site

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.android
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.viewmodels.InsultViewModel
 
@@ -14,20 +17,31 @@ class MainActivity : AppCompatActivity() {
     private val toolbar: Toolbar? by lazy { findViewById<Toolbar?>(R.id.toolbar) }
 
     private val insultViewModel: InsultViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        try {
-            insultViewModel.insultGenerator()
-        } catch (e: Exception) {
-            e.printStackTrace()
+        val btnGenerate: Button = findViewById(R.id.generate_btn)
+        val textInsult: TextView = findViewById(R.id.insult_text)
+        btnGenerate.setOnClickListener {
+            btnGenerate.isEnabled = false
+            insultViewModel.generateInsult()
         }
-        Log.d("daniel", "Entro la aplicación")
+        insultViewModel.observe(this) {
+            textInsult.text = insultViewModel.insult
+            btnGenerate.isEnabled = true
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_activity_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        insultViewModel.destroy(this)
+    }
+
 }
--- app/src/main/java/com/example/android/viewmodels/InsultViewModel.kt
+++ app/src/main/java/com/example/android/viewmodels/InsultViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.android.viewmodels
 
+import android.app.Activity
+import android.content.Context
 import android.util.Log
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.widget.Button
+import androidx.lifecycle.*
+import com.example.android.MainActivity
+import com.example.android.R
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -10,10 +17,16 @@ import java.net.URL
 
 class InsultViewModel : ViewModel() {
 
-    val insult: MutableLiveData<String> = MutableLiveData()
+    private val insultData: MutableLiveData<String> by lazy { MutableLiveData<String>() }
 
+    val insult: String?
+        get() = insultData.value
 
-    fun insultGenerator() {
+    fun generateInsult() {
+        viewModelScope.launch { connectHttps() }
+    }
+
+    private suspend fun connectHttps() = withContext(IO) {
         val url: URL
         var urlConnection: HttpURLConnection? = null
         try {
@@ -23,19 +36,31 @@ class InsultViewModel : ViewModel() {
             val `in`: InputStream = urlConnection.inputStream
             val isw = InputStreamReader(`in`)
             var data: Int = isw.read()
+            var aux = ""
             while (data != -1) {
                 val current = data.toChar()
                 data = isw.read()
-                Log.d("daniel", "Current$current")
+                aux += current
             }
+            insultData.postValue(aux)
         } catch (e: Exception) {
             e.printStackTrace()
             Log.d("daniel", "catch")
         } finally {
             urlConnection?.disconnect()
-            Log.d("daniel", "Disconnect")
         }
     }
 
-//cancion minuto 25, faltando 17 para terminar
+    fun observe(owner: LifecycleOwner, onUpdate: (String) -> Unit) {
+        insultData.observe(owner, Observer { onUpdate(it) })
+    }
+
+    fun destroy(owner: LifecycleOwner) {
+        insultData.removeObservers(owner)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+
 }
