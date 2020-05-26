7e17a83e027fbb892db5fe4a09abc9f5ff8282bf
Daniel-Lagos01 <d.f.l.m.0126@gmail.com>
2020-05-25 22:22:55 -0500

    Change enum class and add 2 parameters

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.android
 
+
+import android.content.DialogInterface
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.android.viewmodels.InsultViewModel
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
 class MainActivity : AppCompatActivity() {
 
     private val toolbar: Toolbar? by lazy { findViewById<Toolbar?>(R.id.toolbar) }
 
     private val insultViewModel: InsultViewModel by viewModels()
+
+    private var alertDialog: AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +37,31 @@ class MainActivity : AppCompatActivity() {
             textInsult.text = insultViewModel.insult
             btnGenerate.isEnabled = true
         }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_activity_menu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.btn_translate -> {
+                /*   for (userEnum in Language.values()) {
+                    Log.d("daniel", userEnum.name)
+                }*/
+                alertDialog = MaterialAlertDialogBuilder(this).setTitle("LANGUAGE")
+                    .setItems(
+                        //arrayOf(Language.values()).map { it[] }.toTypedArray(),
+                        DialogInterface.OnClickListener { dialog, which ->
+
+                        })
+                    .show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onDestroy() {
