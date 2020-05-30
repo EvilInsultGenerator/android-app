cc8cb70a9b003108bdfe184216ce224c2c0caceb
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-05-29 20:58:54 -0500

    Code cleanup in InsultViewModel

--- app/src/main/kotlin/com/evilinsult/android/app/viewmodels/InsultViewModel.kt
+++ app/src/main/kotlin/com/evilinsult/android/app/viewmodels/InsultViewModel.kt
@@ -3,7 +3,11 @@ package com.evilinsult.android.app.viewmodels
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.lifecycle.*
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -14,14 +18,14 @@ class InsultViewModel(application: Application) : AndroidViewModel(application) 
     private val insultData: MutableLiveData<String> by lazy { MutableLiveData<String>() }
 
     private val prefs: SharedPreferences by lazy {
-        application.getSharedPreferences(LANGUAGE_KEY, Context.MODE_PRIVATE)
+        application.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
     }
 
     val insult: String
         get() = insultData.value.orEmpty().trim()
 
     val currentLanguageCode: String
-        get() = prefs.getString(LANGUAGE_KEY, "en") ?: "en"
+        get() = prefs.getString(LANGUAGE_PREF_KEY, "en") ?: "en"
 
     private val insultUrl: String
         get() = "https://evilinsult.com/generate_insult.php?lang=$currentLanguageCode"
@@ -51,7 +55,7 @@ class InsultViewModel(application: Application) : AndroidViewModel(application) 
         val selectedLanguageCode = Language.values()[selectedOption].languageCode
         if (selectedLanguageCode != currentLanguageCode) {
             with(prefs.edit()) {
-                putString(LANGUAGE_KEY, selectedLanguageCode)
+                putString(LANGUAGE_PREF_KEY, selectedLanguageCode)
                 apply()
             }
             generateInsult()
@@ -67,6 +71,7 @@ class InsultViewModel(application: Application) : AndroidViewModel(application) 
     }
 
     companion object {
-        internal const val LANGUAGE_KEY = "LANGUAGES"
+        private const val PREFERENCES_NAME = "evil_insult_generator_prefs"
+        private const val LANGUAGE_PREF_KEY = "current_lang"
     }
 }
