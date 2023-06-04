4dc207a02b1df57272568977b6c2a36c71701c56
Ricardo Souza <souzaariick@gmail.com>
2023-06-04 18:15:24 -0300

    DEVKOTLIN (#130)
    * DEVKOTLIN
    
    Removed: <uses-feature android:name="android.hardware.type.watch" />
    
    Fixed: Build on development Branch
    
    * Fixed: JDK 17 that is needed to build AGP 8.0

--- app/src/main/kotlin/com/evilinsult/extensions/Toolbar.kt
+++ app/src/main/kotlin/com/evilinsult/extensions/Toolbar.kt
@@ -11,13 +11,13 @@ import com.evilinsult.R
 fun Toolbar.tintMenu(
     @ColorInt color: Int =
         context.resolveColor(
-            R.attr.colorOnPrimary,
+            com.google.android.material.R.attr.colorOnPrimary,
             ContextCompat.getColor(context, R.color.onPrimary)
         )
 ) {
     overflowIcon?.tint(color)
     @SuppressLint("PrivateResource")
-    val overflowDescription = context.getString(R.string.abc_action_menu_overflow_description)
+    val overflowDescription = context.getString(androidx.appcompat.R.string.abc_action_menu_overflow_description)
     val outViews = ArrayList<View>()
     findViewsWithText(outViews, overflowDescription, View.FIND_VIEWS_WITH_CONTENT_DESCRIPTION)
     if (outViews.isEmpty()) return
