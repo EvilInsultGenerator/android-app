f463b24cd21e101814d53f8e267ea153c23f9b1f
Ricardo Souza <souzaariick@gmail.com>
2023-06-05 12:20:17 -0300

    Upgrade gradle buildfiles to new kts way

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("com.android.application") version "8.0.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+}
+
+
+tasks.register<Delete>("clean").configure {
+    delete(rootProject.buildDir)
+}
