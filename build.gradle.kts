b32223f16104dce5e3053804e295c30d9c8db96c
Marvin <marvin@evilinsult.com>
2024-08-25 20:37:42 +0200

    Update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.4.0" apply false
+    id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
